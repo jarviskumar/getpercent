6d5565b266e35c45cb9b553beb02854ac6b800da
Dipesh <131427812+jarviskumar@users.noreply.github.com>
2024-09-19 09:27:58 +0530

    Update App.js
    add confetti logic

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-
- var obtain = document.getElementById('obtainMark');
-    var total = document.getElementById('totalMark');
-    //  var btn = document.getElementById('calculate');
-    var perCent = document.getElementById('per')
-
-    calcbtn.onclick = function () {
-      let x = (obtain.value / total.value) * 100;
-      x = parseInt(x);
-      per.innerHTML = x + "%"
+  function getPercent() {
+       let obtain = document.getElementById('obtainMark').value;
+       let total = document.getElementById('totalMark').value;
+       let errorBox = document.getElementById("errorBox");
+       let x = (obtain / total) * 100;
+       x = Number.parseInt(x);
+      
+      
+      if(obtain>0 && total >0){
+        let perCent = document.getElementById('per');
+        per.innerHTML = x + "%";
+        errorBox.innerHTML = ""
+      confettiFunc()
+      }else{
+         errorBox.innerHTML = "Enter Valid Number";
+         per.innerHTML = "%"
+      }
     };
+    
+    function confettiFunc(){
+     confetti({
+       particleCount:200,
+       spread:100,
+       origin: {y: 0.6}
+     });
+    }
+    
+   let calcbtn = document.getElementById("calcbtn");
+   calcbtn.addEventListener("click",getPercent)
